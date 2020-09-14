8f3a4d1277004e838971a62b13a453168ea25b2a
Umang2002 <56287922+Umang2002@users.noreply.github.com>
2020-09-14 15:09:21 +0530

    Update headRecursion.cpp

--- headRecursion.cpp
+++ headRecursion.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 #include <string.h>
-
 using namespace std;
-
 void fun1(int n){
-
 if(n > 0){
-
     fun1(n-1);
     cout << n <<endl;
-
-}
-}
-
+}}
 int main(){
   int x;
   cin >> x;
